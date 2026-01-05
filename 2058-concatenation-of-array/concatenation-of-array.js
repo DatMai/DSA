ab2fc766b2b4df73a8de0d1185d64ec7c27eaf0f
Mai Huynh Phuoc Dat <74104677+DatMai@users.noreply.github.com>
2026-01-05 09:18:35 +0700

    Time: 1 ms (56.35%) | Memory: 59.7 MB (5.65%) - LeetSync

---
+++ 2058-concatenation-of-array/concatenation-of-array.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var getConcatenation = function (nums) {
+    let ans = []
+    for (let i = 0; i < nums.length * 2; i++) {
+        ans[i] = nums[i];
+        if (!nums[i]) {
+            ans[i] = nums[i - nums.length];
+        }
+    }
+    return ans;
+};
