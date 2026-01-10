5d62c14ab109a2d6307bc1fa235ec1cde9316b01
Mai Huynh Phuoc Dat <74104677+DatMai@users.noreply.github.com>
2026-01-10 19:14:21 +0700

    Time: 497 ms (6.87%) | Memory: 68.8 MB (5.16%) - LeetSync

--- 1016-subarray-sums-divisible-by-k/subarray-sums-divisible-by-k.js
+++ 1016-subarray-sums-divisible-by-k/subarray-sums-divisible-by-k.js
@@ -8,10 +8,11 @@ var subarraysDivByK = function (nums, k) {
     let sum = 0;
     let map = new Map();
     map.set(0, 1);
+    console.log('log to see why we need to use a long expression instead of sum % k', -1 % 2, ((-1 % 2) + 2) % 2)
     for (let i = 0; i < nums.length; i++) {
         sum += nums[i];
         r = ((sum % k) + k) % k;
-        
+        console.log('checking r', r, sum % k);
         if (map.has(r)) {
             count += map.get(r);
         }
@@ -19,9 +20,3 @@ var subarraysDivByK = function (nums, k) {
     }
     return count;
 };
-
-// // prefixSum(i) - k = prefixSum(j) => prefixSum(j) % k === 0 -> correct
-//   0  1  2  3  4 5 
-//   4  5  0 -2 -3 1   , k=5 
-// 0 4  9  9  7  4 5
-// 1 1  1  2  1  2 1
