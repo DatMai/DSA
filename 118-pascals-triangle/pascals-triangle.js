f1ec57ab09dcf79cc29ec3fb4b3f80d8a4ddd315
Mai Huynh Phuoc Dat <74104677+DatMai@users.noreply.github.com>
2026-01-24 21:09:06 +0700

    Time: 1 ms (24.77%) | Memory: 54.9 MB (57.42%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.js
+++ 118-pascals-triangle/pascals-triangle.js
@@ -4,14 +4,15 @@
  */
 var generate = function (numRows) {
     let arr = [[1]];
-
+    
     for (let i = 1; i < numRows; i++) {
         arr.push(new Array(i + 1).fill(0));
 
         for (let j = 0; j < i + 1; j++) {
-            arr[i][j] += j > 0 ? arr[i - 1][j-1] : 0;
-            arr[i][j] += j < i ? arr[i - 1][j] : 0
+            arr[i][j] = j > 0 ? arr[i - 1][j - 1] : 0;
+            arr[i][j] += j < i ? arr[i - 1][j] : 0;
         }
     }
+
     return arr;
 };
