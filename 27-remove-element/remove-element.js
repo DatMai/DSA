1bc2ec43d13c1305d67aa72051544f4ec03427c6
Mai Huynh Phuoc Dat <74104677+DatMai@users.noreply.github.com>
2026-01-31 21:22:20 +0700

    Time: 0 ms (100.00%) | Memory: 53.8 MB (66.30%) - LeetSync

--- 27-remove-element/remove-element.js
+++ 27-remove-element/remove-element.js
@@ -5,7 +5,7 @@
  */
 var removeElement = function (nums, val) {
     let count = 0;
-    for (let i in nums) {
+    for (let i = 0; i < nums.length; ++i) {
         if (nums[i] != val) {
             nums[count] = nums[i];
             count++;
