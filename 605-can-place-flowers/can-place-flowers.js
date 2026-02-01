7fb9c7000e2a68944d5c96412ba376670fde055a
Mai Huynh Phuoc Dat <74104677+DatMai@users.noreply.github.com>
2026-02-01 11:04:21 +0700

    Time: 10 ms (6.97%) | Memory: 55.5 MB (87.67%) - LeetSync

---
+++ 605-can-place-flowers/can-place-flowers.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} flowerbed
+ * @param {number} n
+ * @return {boolean}
+ */
+var canPlaceFlowers = function (flowerbed, n) {
+    let count = 0;
+    for (let i = 0; i < flowerbed.length; ++i) {
+        if (flowerbed[i] == 0 && (flowerbed[i - 1] == 0 || flowerbed[i - 1] == null) && (flowerbed[i + 1] == 0 || flowerbed[i + 1] == null)) {
+            count++;
+            i = i + 1;
+        }
+    }
+    console.log('checking count', count, flowerbed);
+    return n <= count;
+};
