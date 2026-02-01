944628d5be2b4d50f499f46b4ed2ada664da7e9b
Mai Huynh Phuoc Dat <74104677+DatMai@users.noreply.github.com>
2026-02-01 11:21:04 +0700

    Time: 2 ms (33.33%) | Memory: 56 MB (100.00%) - LeetSync

--- 3263-divide-an-array-into-subarrays-with-minimum-cost-i/divide-an-array-into-subarrays-with-minimum-cost-i.ts
+++ 3263-divide-an-array-into-subarrays-with-minimum-cost-i/divide-an-array-into-subarrays-with-minimum-cost-i.ts
@@ -1,4 +1,5 @@
 function minimumCost(nums: number[]): number {
-    nums = [nums[0], ...nums.slice(1).sort((a, b) => a - b)];
+    const tail = nums.slice(1).sort((a, b) => a - b);
+    nums.splice(1, nums.length - 1, ...tail);
     return nums[0] + nums[1] + nums[2];
-};
+}
