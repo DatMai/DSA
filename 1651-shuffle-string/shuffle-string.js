432c4a617b40490c7e8515ce0da865ff5bd7a89b
Mai Huynh Phuoc Dat <74104677+DatMai@users.noreply.github.com>
2026-02-02 12:33:48 +0700

    Time: 0 ms (100.00%) | Memory: 58 MB (6.67%) - LeetSync

---
+++ 1651-shuffle-string/shuffle-string.js
@@ -0,0 +1,13 @@
+/**
+ * @param {string} s
+ * @param {number[]} indices
+ * @return {string}
+ */
+var restoreString = function (s, indices) {
+    let newArr = new Array(indices.length);
+    s.split('');
+    for (let i = 0; i < s.length; ++i) {
+        newArr[indices[i]] = (s[i]);
+    }
+    return newArr.join('');
+};
