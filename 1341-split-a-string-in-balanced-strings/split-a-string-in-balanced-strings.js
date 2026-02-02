2d4e3c64f9b19be0f0155e0c6ddb628c30c9baa2
Mai Huynh Phuoc Dat <74104677+DatMai@users.noreply.github.com>
2026-02-02 12:43:11 +0700

    Time: 0 ms (100.00%) | Memory: 55.3 MB (18.25%) - LeetSync

---
+++ 1341-split-a-string-in-balanced-strings/split-a-string-in-balanced-strings.js
@@ -0,0 +1,13 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var balancedStringSplit = function (s) {
+    let count = 0, sum = 0;
+    for (let i = 0; i < s.length; ++i) {
+        if (s[i] == 'L') sum++;
+        if (s[i] == 'R') sum--;
+        if (sum == 0) count++;
+    }
+    return count;
+};
