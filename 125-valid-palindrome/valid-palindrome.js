6de402ba4d711f67f16591bbada9f30905ab18ac
Mai Huynh Phuoc Dat <74104677+DatMai@users.noreply.github.com>
2026-02-02 10:46:50 +0700

    Time: 6 ms (50.45%) | Memory: 61.9 MB (16.92%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.js
@@ -0,0 +1,14 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isPalindrome = function (s) {
+    let newString = '';
+    for (let c of s) {
+        if ((c >= 0 && c <= 9 || c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z') && c != ' ') {
+            newString += c.toLowerCase();
+        };
+    }
+    let reversedString = newString.split('').reverse().join('')
+    return newString === reversedString;;
+};
