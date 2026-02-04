d700ba9640254a91a17e9e840d7f05ab995d88a7
Mai Huynh Phuoc Dat <74104677+DatMai@users.noreply.github.com>
2026-02-04 12:56:55 +0700

    Time: 0 ms (100.00%) | Memory: 53.7 MB (60.78%) - LeetSync

--- 2361-calculate-digit-sum-of-a-string/calculate-digit-sum-of-a-string.js
+++ 2361-calculate-digit-sum-of-a-string/calculate-digit-sum-of-a-string.js
@@ -1,42 +1,14 @@
-/**
- * @param {string} s
- * @param {number} k
- * @return {string}
- */
 var digitSum = function (s, k) {
-    let size = s.length;
-    let arr = [];
-    if (s.length <= k) return s;
-
-    while (size > k) {
-        let a = arr.length > 0 ? arr.join('') : s;
-        arr = divine(a, k);
-        size = arr.length;
-    }
-
-    return arr.join('');
-};
-
-divine = (s, k) => {
-    let arr = [];
-    for (let i = 0; i < s.length; ++i) {
-        let subArr = [];
-        for (let j = i; j < i + k; ++j) {
-            if (s[j]) subArr.push(s[j]);
+    while (s.length > k) {
+        let res = '';
+        for (let i = 0; i < s.length; i += k) {
+            let sum = 0;
+            for (let j = i; j < i + k && j < s.length; j++) {
+                sum += s[j] - '0';
+            }
+            res += sum;
         }
-
-        arr.push(subArr);
-        i = i + k - 1;
+        s = res;
     }
-
-    arr = merge(arr);
-    arr = arr.join('').split('');
-    return arr;
-}
-
-merge = (arr) => {
-    for (let i = 0; i < arr.length; ++i) {
-        arr[i] = arr[i].reduce((e, v) => Number(e) + Number(v), 0);
-    }
-    return arr;
-}
+    return s;
+};
