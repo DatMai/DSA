2fd97e77802b6b7c1fc92d5e97a6a9835b9722b5
Mai Huynh Phuoc Dat <74104677+DatMai@users.noreply.github.com>
2026-02-04 13:58:58 +0700

    Time: 5 ms (57.29%) | Memory: 57.3 MB (41.72%) - LeetSync

---
+++ 541-reverse-string-ii/reverse-string-ii.js
@@ -0,0 +1,17 @@
+/**
+ * @param {string} s
+ * @param {number} k
+ * @return {string}
+ */
+var reverseStr = function (s, k) {
+    let a = s.split('');
+    for (let start = 0; start < a.length; start += 2 * k) {
+        let i = start, j = Math.min(start + k - 1, a.length - 1);
+        while (i < j) {
+            let temp = a[i];
+            a[i++] = a[j];
+            a[j--] = temp;
+        }
+    }
+    return a.join('');
+};
