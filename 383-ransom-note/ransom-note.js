8beaa660b6f744ef3089e786eeed7df97c8eeb0c
Mai Huynh Phuoc Dat <74104677+DatMai@users.noreply.github.com>
2026-02-05 16:32:03 +0700

    Time: 3 ms (98.80%) | Memory: 60.7 MB (22.26%) - LeetSync

--- 383-ransom-note/ransom-note.js
+++ 383-ransom-note/ransom-note.js
@@ -1,9 +1,18 @@
+/**
+ * @param {string} ransomNote
+ * @param {string} magazine
+ * @return {boolean}
+ */
 var canConstruct = function (ransomNote, magazine) {
     for (let i = 0; i < ransomNote.length; ++i) {
-        const idx = magazine.indexOf(ransomNote[i]);
-        if (idx === -1) return false;
-
-        magazine = magazine.slice(0, idx) + magazine.slice(idx + 1);
+        let index = magazine.indexOf(ransomNote[i]);
+        if (index == -1) {
+            return false;
+        }
+        else {
+            magazine = magazine.slice(0, index) + magazine.slice(index + 1);
+        };
     }
+
     return true;
 };
