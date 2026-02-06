c7b229170a581779fc289ab8f85a8fd45e6a6dc9
Mai Huynh Phuoc Dat <74104677+DatMai@users.noreply.github.com>
2026-02-06 23:37:03 +0700

    Time: 0 ms (100.00%) | Memory: 55.9 MB (38.86%) - LeetSync

--- 202-happy-number/happy-number.js
+++ 202-happy-number/happy-number.js
@@ -1,14 +1,18 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
 var isHappy = function (n) {
     return dfs(n, getNext(n));
 };
 
-function dfs(slow, fast) {
+dfs = (slow, fast) => {
     if (fast === 1) return true;
     if (slow === fast) return false;
     return dfs(getNext(slow), getNext(getNext(fast)));
 }
 
-function getNext(n) {
+getNext = (n) => {
     let sum = 0;
     while (n > 0) {
         let digit = n % 10;
