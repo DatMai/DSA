e5c5b970abd657c01934965a1abe9fb64b364dfe
Mai Huynh Phuoc Dat <74104677+DatMai@users.noreply.github.com>
2026-02-28 22:27:08 +0700

    Time: 578 ms (2.11%) | Memory: 55.8 MB (6.32%) - LeetSync

--- 319-bulb-switcher/bulb-switcher.js
+++ 319-bulb-switcher/bulb-switcher.js
@@ -4,7 +4,6 @@
  */
 var bulbSwitch = function (n) {
     let count = 0;
-    if (n === 1) return 1;
     for (let i = 1; i <= n; ++i) {
         let r = Math.floor(Math.sqrt(i));
         if (r * r == i) count++;
