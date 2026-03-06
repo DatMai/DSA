cae5f424507341fa2225154842aee93991be454c
Mai Huynh Phuoc Dat <74104677+DatMai@users.noreply.github.com>
2026-03-06 10:21:33 +0700

    Time: 1 ms (91.46%) | Memory: 57.4 MB (69.00%) - LeetSync

--- 724-find-pivot-index/find-pivot-index.js
+++ 724-find-pivot-index/find-pivot-index.js
@@ -5,18 +5,17 @@
 var pivotIndex = function (nums) {
     let totalSum = 0;
     let leftSum = 0;
-    let pivotIndex = -1;
     for (let i = 0; i < nums.length; ++i) {
         totalSum += nums[i];
     }
 
     for (let i = 0; i < nums.length; ++i) {
-        leftSum += nums[i - 1] ? nums[i - 1] : 0;
         if (leftSum == totalSum - nums[i] - leftSum) {
-            pivotIndex = i;
-            break;
+            return i;
         }
+
+        leftSum += nums[i]
     }
 
-    return pivotIndex;
+    return -1;
 };
