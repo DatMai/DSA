ea0b928fbee0a81f6524a3d18ea59cffca447081
Mai Huynh Phuoc Dat <74104677+DatMai@users.noreply.github.com>
2026-03-09 23:08:58 +0700

    Time: 181 ms (31.90%) | Memory: 0B (100.00%) - LeetSync

--- 1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
+++ 1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
@@ -1,9 +1,6 @@
-SELECT a.machine_id,
-       ROUND(CAST(AVG(b.timestamp - a.timestamp) AS numeric), 3) AS processing_time
-FROM Activity a
-JOIN Activity b
-  ON a.machine_id = b.machine_id
- AND a.process_id = b.process_id
-WHERE a.activity_type = 'start'
-  AND b.activity_type = 'end'
-GROUP BY a.machine_id;
+SELECT a1.machine_id, ROUND(CAST(AVG(a2.timestamp - a1.timestamp) as numeric), 3) as processing_time
+FROM activity a1
+JOIN activity a2 
+ON a1.machine_id = a2.machine_id AND a1.process_id = a2.process_id
+WHERE a1.activity_type = 'start' AND a2.activity_type = 'end'
+GROUP BY a1.machine_id;
