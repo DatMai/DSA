715b6be0b450dbff60272ce341f18bfe4c83b216
Mai Huynh Phuoc Dat <74104677+DatMai@users.noreply.github.com>
2026-03-22 23:16:07 +0700

    Time: 23 ms (5.71%) | Memory: 58.9 MB (23.41%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.js
+++ 36-valid-sudoku/valid-sudoku.js
@@ -3,51 +3,23 @@
  * @return {boolean}
  */
 var isValidSudoku = function (board) {
-    let subStartPositionSudoku = [
-        [0, 0], [0, 3], [0, 6],
-        [3, 0], [3, 3], [3, 6],
-        [6, 0], [6, 3], [6, 6],
-    ];
+    let seen = new Set();
+    for (let i = 0; i < 9; ++i) {
+        for (let j = 0; j < 9; ++j) {
+            if (board[i][j] == '.') continue;
+            let row = `${board[i][j]} in row ${i}`;
+            let col = `${board[i][j]} in col ${j}`;
+            let box = `${board[i][j]} in box (${Math.floor(i / 3)},${Math.floor(j / 3)})`;
 
-    for (let i = 0; i < board.length; ++i) {
-        let rowCache = new Map();
-        for (let j = 0; j < board[i].length; ++j) {
-            if (!rowCache.has(board[i][j]) && board[i][j] != '.') {
-                rowCache.set(board[i][j], board[i][j]);
-            } else if (board[i][j] != '.') {
-                return false;
+            if (seen.has(row) || seen.has(col) || seen.has(box)) {
+                console.log('checking seen', row, col, box, seen)
+                return false
+            };
 
-            }
+            seen.add(row);
+            seen.add(col);
+            seen.add(box);
         }
     }
-
-    for (let i = 0; i < board.length; ++i) {
-        let columnCache = new Map();
-        for (let j = 0; j < board[i].length; ++j) {
-            if (!columnCache.has(board[j][i]) && board[j][i] != '.') {
-                columnCache.set(board[j][i], board[j][i]);
-            } else if (board[j][i] != '.') {
-                return false;
-            }
-        }
-    }
-
-    for (let s = 0; s < subStartPositionSudoku.length; ++s) {
-        let init_r = subStartPositionSudoku[s][0];
-        let init_c = subStartPositionSudoku[s][1];
-
-        let subSudokuCache = new Map();
-        for (let i = init_r; i < init_r + 3; ++i) {
-            for (let j = init_c; j < init_c + 3; ++j) {
-                if (!subSudokuCache.has(board[i][j]) && board[i][j] != '.') {
-                    subSudokuCache.set(board[i][j]);
-                }
-                else if (board[i][j] != '.') {
-                    return false;
-                }
-            }
-        }
-    }
-
     return true;
 };
