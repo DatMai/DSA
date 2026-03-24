f59fa39fe5f4ac2316fa6bf363822a179681790c
Mai Huynh Phuoc Dat <74104677+DatMai@users.noreply.github.com>
2026-03-24 18:37:12 +0700

    Time: 0 ms (100.00%) | Memory: 57.1 MB (36.52%) - LeetSync

--- 3263-divide-an-array-into-subarrays-with-minimum-cost-i/divide-an-array-into-subarrays-with-minimum-cost-i.js
+++ 3263-divide-an-array-into-subarrays-with-minimum-cost-i/divide-an-array-into-subarrays-with-minimum-cost-i.js
@@ -3,14 +3,17 @@
  * @return {number}
  */
 var minimumCost = function (nums) {
-    let min1 = 51, min2 = 52;
+    let min1 = Infinity, min2 = Infinity;
+
     for (let i = 1; i < nums.length; ++i) {
-        if (nums[i] < min1) {
+        if (min1 > nums[i]) {
             min2 = min1;
             min1 = nums[i];
-        } else if (min2 > nums[i]) {
+        } else if (nums[i] < min2) {
             min2 = nums[i];
         }
+
     }
+
     return nums[0] + min1 + min2;
 };
