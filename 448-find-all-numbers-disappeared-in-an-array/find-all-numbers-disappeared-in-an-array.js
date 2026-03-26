dee24eb55564a402212555a0cf01ed9efc3c7fbc
Mai Huynh Phuoc Dat <74104677+DatMai@users.noreply.github.com>
2026-03-27 00:11:12 +0700

    Time: 8 ms (76.62%) | Memory: 72 MB (85.61%) - LeetSync

--- 448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.js
+++ 448-find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.js
@@ -4,17 +4,15 @@
  */
 var findDisappearedNumbers = function (nums) {
     let result = [];
-    for (let i = 0; i < nums.length; i++) {
+    for (let i = 0; i < nums.length; ++i) {
         let newIndex = Math.abs(nums[i]) - 1;
         if (nums[newIndex] > 0) {
             nums[newIndex] = -nums[newIndex];
         }
     }
 
-    for (let i = 0; i < nums.length; i++) {
-        if (nums[i] > 0) {
-            result.push(i + 1);
-        }
+    for (let i = 0; i < nums.length; ++i) {
+        if (nums[i] > 0) result.push(i + 1);
     }
 
     return result;
