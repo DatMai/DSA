f7ff95814cba5d5a67dd1459fe2546d7cc9f80b7
Mai Huynh Phuoc Dat <74104677+DatMai@users.noreply.github.com>
2026-03-26 23:40:57 +0700

    Time: 4 ms (35.04%) | Memory: 65.4 MB (52.72%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.js
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.js
@@ -3,13 +3,14 @@
  * @return {number}
  */
 var maxProfit = function (prices) {
-    let profit = 0;
     let minPrice = prices[0];
-
-    for (let i = 0; i < prices.length; ++i) {
-        if (prices[i] < minPrice) minPrice = prices[i];
-        profit = Math.max(profit, prices[i] - minPrice);
+    let maxProfit = 0;
+    for (let i = 1; i < prices.length; ++i) {
+        if (prices[i] < minPrice) {
+        minPrice = prices[i];
+        }
+        maxProfit = Math.max(maxProfit, prices[i] - minPrice);
     }
 
-    return profit;
+    return maxProfit;
 };
