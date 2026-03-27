4757a9dff02a557254a39e63cd99a597b5f064b9
Mai Huynh Phuoc Dat <74104677+DatMai@users.noreply.github.com>
2026-03-27 09:24:17 +0700

    Time: 0 ms (100.00%) | Memory: 58 MB (29.99%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.js
+++ 8-string-to-integer-atoi/string-to-integer-atoi.js
@@ -19,12 +19,12 @@ var myAtoi = function (s) {
     let result = 0;
     const INT_MAX = (2 ** 31) - 1;
     const INT_MIN = -(2 ** 31)
+    const MAX_DIV_10 = Math.floor(INT_MAX / 10);
+    const MAX_LAST_DIGIT = INT_MAX % 10; // = 7
     while (i < n && s[i] >= '0' && s[i] <= '9') {
         let digit = s[i] - '0';
 
         // check overflow before adding digit
-        const MAX_DIV_10 = Math.floor(INT_MAX / 10);
-        const MAX_LAST_DIGIT = INT_MAX % 10; // = 7
         if (result > MAX_DIV_10 || (result === MAX_DIV_10 && digit > MAX_LAST_DIGIT)) {
             return sign === 1 ? INT_MAX : INT_MIN;
         }
