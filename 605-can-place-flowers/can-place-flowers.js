125557c7507c6b2ea16515873e6500e20ef9067d
Mai Huynh Phuoc Dat <74104677+DatMai@users.noreply.github.com>
2026-04-04 23:44:41 +0700

    Time: 3 ms (34.78%) | Memory: 57.5 MB (26.28%) - LeetSync

--- 605-can-place-flowers/can-place-flowers.js
+++ 605-can-place-flowers/can-place-flowers.js
@@ -4,18 +4,16 @@
  * @return {boolean}
  */
 var canPlaceFlowers = function (flowerbed, n) {
+    if (flowerbed.length == 1 && flowerbed[0] == 0 && n == 1) return true;
     for (let i = 0; i < flowerbed.length; ++i) {
-        if (flowerbed[i] === 0) {
-            let left = i === 0 || flowerbed[i - 1] === 0;
-            let right = i === flowerbed.length - 1 || flowerbed[i + 1] === 0;
+        let left = i === 0 || flowerbed[i - 1] === 0 ? true : false;
+        let right = i === flowerbed.length - 1 || flowerbed[i + 1] === 0 ? true : false;
 
-            if (left && right && flowerbed[i] === 0) {
-                flowerbed[i] = 1;
-                n--;
-            }
+        if (left && right && flowerbed[i] === 0) {
+            flowerbed[i] = 1;
+            n--;
         }
     }
-    if (flowerbed.length === 1 && flowerbed[0] === 0) return true;
 
     return n <= 0;
 };
