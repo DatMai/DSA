e79ebd398027deca23135f5accc2cd5e580c828a
Mai Huynh Phuoc Dat <74104677+DatMai@users.noreply.github.com>
2026-04-05 09:42:44 +0700

    Time: 6 ms (42.92%) | Memory: 57.6 MB (50.95%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.js
+++ 36-valid-sudoku/valid-sudoku.js
@@ -1,22 +1,20 @@
+/**
+ * @param {character[][]} board
+ * @return {boolean}
+ */
 var isValidSudoku = function (board) {
     let set = new Set();
-
     for (let i = 0; i < 9; ++i) {
         for (let j = 0; j < 9; ++j) {
             if (board[i][j] === '.') continue;
-
-            let num = board[i][j];
-
-            let row = `${num} in row ${i}`;
-            let col = `${num} in col ${j}`;
-            let box = `${num} in box ${Math.floor(i / 3)}-${Math.floor(j / 3)}`;
-
-            if (set.has(row) || set.has(col) || set.has(box)) {
-                return false;
-            }
+            let row = `${board[i][j]} in r ${i}`;
+            let column = `${board[i][j]} in c ${j}`;
+            let box = `${board[i][j]} in b ${Math.floor(i / 3)}, ${Math.floor(j / 3)}`;
+        
+            if (set.has(row) || set.has(column) || set.has(box)) return false;
 
             set.add(row);
-            set.add(col);
+            set.add(column);
             set.add(box);
         }
     }
