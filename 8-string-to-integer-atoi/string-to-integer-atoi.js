6bfedf8468de187abb9ec97392a58ee1ce7d69a5
Mai Huynh Phuoc Dat <74104677+DatMai@users.noreply.github.com>
2026-04-06 12:42:23 +0700

    Time: 1 ms (87.84%) | Memory: 57.8 MB (36.85%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.js
+++ 8-string-to-integer-atoi/string-to-integer-atoi.js
@@ -3,31 +3,31 @@
  * @return {number}
  */
 var myAtoi = function (s) {
-    let n = s.length;
     let i = 0;
+    let n = s.length;
 
-    while (i< n && s[i] === ' ') {
-        ++i;
+    while (i < n && s[i] == ' ') {
+        i++;
     }
 
     let sign = 1;
     if (i < n && (s[i] === '+' || s[i] === '-')) {
-        if (s[i] === '-') { sign = -1; }
-        ++i;
+        if (s[i] === '-') sign = -1;
+        i++;
     }
 
     let result = 0;
     let MAX_INT = (2 ** 31) - 1;
     let MIN_INT = -(2 ** 31);
     let MAX_DIV_10 = Math.floor(MAX_INT / 10);
-    let MAX_LAST_DIGIT = MAX_INT % 10;
+    let LAST_MAX_DIGIT = MAX_INT % 10;
     while (i < n && s[i] >= '0' && s[i] <= '9') {
         let digit = s[i] - '0';
-        if (result > MAX_DIV_10 || (result === MAX_DIV_10 && digit > MAX_LAST_DIGIT)) {
+        if (MAX_DIV_10 < result || (MAX_DIV_10 === result && LAST_MAX_DIGIT < digit)) {
             return sign === 1 ? MAX_INT : MIN_INT;
         }
         result = result * 10 + digit;
-        ++i;
+        i++;
     }
 
     return result * sign;
