2848e5f42869b11af310ad3eb8381a6fb1d1553a
Mai Huynh Phuoc Dat <74104677+DatMai@users.noreply.github.com>
2026-04-22 13:00:21 +0700

    Time: 2 ms (94.29%) | Memory: 58.6 MB (27.17%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.js
+++ 36-valid-sudoku/valid-sudoku.js
@@ -3,21 +3,24 @@
  * @return {boolean}
  */
 var isValidSudoku = function (board) {
-    let set = new Set();
+    let sodokuSet = new Set();
     for (let i = 0; i < 9; ++i) {
-        for (let j = 0; j < 9; ++j) {
+        for (j = 0; j < 9; ++j) {
             if (board[i][j] === '.') continue;
-            let row = `${board[i][j]} in r ${i}`;
-            let column = `${board[i][j]} in c ${j}`;
-            let box = `${board[i][j]} in b ${Math.floor(i / 3)}, ${Math.floor(j / 3)}`;
-        
-            if (set.has(row) || set.has(column) || set.has(box)) return false;
 
-            set.add(row);
-            set.add(column);
-            set.add(box);
+            let row = `${board[i][j]} in row ${i}`;
+            let column = `${board[i][j]} in column ${j}`;
+            let box = `${board[i][j]} in box (${Math.floor(i / 3)},${Math.floor(j / 3)})`;
+
+            if (sodokuSet.has(row) || sodokuSet.has(column) || sodokuSet.has(box)) {
+                return false;
+            }
+
+            sodokuSet.add(row);
+            sodokuSet.add(column);
+            sodokuSet.add(box);
         }
-    }
 
+    }
     return true;
 };
