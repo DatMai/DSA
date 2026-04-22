4cf626cf3712a3fd2b34ceca14082568a41f61e9
Mai Huynh Phuoc Dat <74104677+DatMai@users.noreply.github.com>
2026-04-22 12:34:46 +0700

    Time: 1 ms (92.28%) | Memory: 57.4 MB (65.86%) - LeetSync

--- 724-find-pivot-index/find-pivot-index.js
+++ 724-find-pivot-index/find-pivot-index.js
@@ -3,10 +3,11 @@
  * @return {number}
  */
 var pivotIndex = function (nums) {
-    let leftSum = 0;
     let totalSum = nums.reduce((a, b) => a + b);
+    let leftSum = 0;
     for (let i = 0; i < nums.length; ++i) {
-        if (totalSum - leftSum - nums[i] == leftSum) return i;
+        let rightSum = totalSum - nums[i] - leftSum;
+        if (leftSum === rightSum) return i;
         leftSum += nums[i];
     }
     return -1;
