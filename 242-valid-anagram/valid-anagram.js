63c1819d8ea862a15c061eb084cad31a92891cc9
Dat Mai Huynh Phuoc <74104677+DatMai@users.noreply.github.com>
2026-08-07 22:31:41 +0700

    Time: 19 ms (33.69%) | Memory: 55.9 MB (69.98%) - LeetSync

--- 242-valid-anagram/valid-anagram.js
+++ 242-valid-anagram/valid-anagram.js
@@ -3,9 +3,24 @@
  * @param {string} t
  * @return {boolean}
  */
-var isAnagram = function(s, t) {
-    let sortedS = s.split('').sort().join('');
-    let sortedT = t.split('').sort().join('');
+var isAnagram = function (s, t) {
+    if (t.length != s.length) return false;
 
-    return sortedS == sortedT;
+    let myMap = new Map();
+    for (let i = 0; i < s.length; i++) {
+        myMap.set(s[i], (myMap.get(s[i]) ? myMap.get(s[i]) : 0) + 1);
+
+    }
+
+    for (let i = 0; i < t.length; i++) {
+        if (myMap.has(t[i])) {
+            myMap.set(t[i], myMap.get(t[i]) - 1);
+        }
+    }
+
+    for (const count of myMap.values()) {
+        if (count !== 0) return false;
+    }
+    
+    return true;
 };
